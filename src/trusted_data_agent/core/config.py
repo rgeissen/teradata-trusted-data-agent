6dbb7139ceae8337dee69765313961c1537c6d25
rgeissen <rainer.geissendoerfer@googlemail.com>
2025-08-10 20:02:53 +0200

    Updated config for Parameterless Prompts from the mcp server

--- src/trusted_data_agent/core/config.py
+++ src/trusted_data_agent/core/config.py
@@ -14,9 +14,9 @@ class AppConfig:
     CURRENT_MODEL_PROVIDER_IN_PROFILE = None
     LLM_API_MAX_RETRIES = 5
     LLM_API_BASE_DELAY = 2 # The base delay in seconds for exponential backoff
-    INITIALLY_DISABLED_PROMPTS = ["qlty_databaseQuality"]
+    INITIALLY_DISABLED_PROMPTS = ["base_tableBusinessDesc", "base_databaseBusinessDesc"]
     # --- NEW: Add a list of tools to be disabled at startup ---
-    INITIALLY_DISABLED_TOOLS = ["rag_executeWorkflow"]
+    INITIALLY_DISABLED_TOOLS = []
 
 APP_CONFIG = AppConfig()
 
